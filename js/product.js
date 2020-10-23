e896e70af6a7cc7117988ef33a28151c4951d536
Philippe Vilarinho <philippebenfica@icloud.com>
2020-10-23 02:54:24 +0200

    Update

--- js/product.js
+++
@@ -1,8 +0,0 @@
-// appel l'api
-fetch ('http://localhost:3000/api/teddies')
-    .then (function (response) {
-        return response.json()
-    }).then (function(data) {
-        console.log (data)
-    })
-
